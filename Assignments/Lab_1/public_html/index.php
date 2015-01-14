3386bb72f054745b4b1dca88c607c43c43b918f9
unknown <dsd12190@CLK310-13.vtc.vsc.edu>
2015-01-13 20:10:07 -0500

    project started

---
+++ Assignments/Lab_1/public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lab 1: Hello World</title>
+</head>
+<body>
+
+	<h1>Just the Basics</h1>
+		<img scr="images.WORLD.jpg">
+		<?php
+			echo "Hello World!";
+		?>
+
+</body>
+</html>
