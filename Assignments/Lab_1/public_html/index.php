6ca0531b08f9b2c310461c52548497f5b9a346ee
Nahrikira <dsd12190@vtc.vsc.edu>
2015-01-19 19:57:51 -0500

    Finished Lab #1

--- Assignments/Lab_1/public_html/index.php
+++ Assignments/Lab_1/public_html/index.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
+<!Doctype html>
 <html>
 <head>
 	<title>Lab 1: Hello World</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<!--Danielle DeLorenzo-->
 </head>
 <body>
 
-	<h1>Just the Basics</h1>
-		<img scr="images.WORLD.jpg">
-		<?php
-			echo "Hello World!";
-		?>
+	<img src="images/earth.jpg" alt="Earth" width="488" height="488">
+	<h1>Hello World!</h1>
+
+	<?php echo "Hello World!";?>
 
 </body>
 </html>
