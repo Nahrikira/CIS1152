a4eb0031977deea3aa2623af39cf5f84ed5fb928
unknown <dsd12190@CLK310-18.vtc.vsc.edu>
2015-02-09 10:49:49 -0500

    Finished Lab 4

--- Assignments/Lab_4/lab_4.php
+++ Assignments/Lab_4/lab_4.php
@@ -6,8 +6,8 @@
  * This lab focuses on your understanding of arithmatic.
  *
  * @version 1.0
- * @author YOUR_NAME <YOUR_EMAIL_ADDRESS@vtc.edu>
- * @since 20150120
+ * @author Danielle_DeLorenzo <dsd12190@vtc.vsc.edu>
+ * @since 20150209
  */
 
 /**
@@ -20,10 +20,15 @@
  */
 
 define('GRAVITY', 9.8);
+$float_value = 0;
+$degrees_f = 0;
+$area = 0;
+$height = 0;
 
 function truncateFloat($float_value)
 {
-
+	$trunc_num = (int)($float_value * 100) / 100;
+	return $trunc_num;
 }
 
 /**
@@ -31,7 +36,8 @@ function truncateFloat($float_value)
  */
 function farenheit2Kelvin($degrees_f)
 {
-
+	$degrees_k = ($degrees_f - 32) * (5/9) + 273.15;
+	return $degrees_k;
 }
 
 /**
@@ -39,7 +45,8 @@ function farenheit2Kelvin($degrees_f)
  */
 function dodecahedronVolume($area)
 {
-
+	$volume = ((15 + 7 * sqrt(5)) / 4) * pow($area, 3);
+	return $volume;
 }
 
 /**
@@ -47,9 +54,40 @@ function dodecahedronVolume($area)
  */
 function impactVelocity($height)
 {
-
+	$velocity = sqrt((2 * GRAVITY * $height));
+	return $velocity;
 }
 
+if (isset($_POST['submit'])) 
+{ 
+	$trunc_num = truncateFloat($_POST['float_value']); 
+	$degrees_k = farenheit2Kelvin($_POST['degrees_f']);
+	$volume = dodecahedronVolume($_POST['area']);
+	$velocity = impactVelocity($_POST['height']);
+} 
+else 
+{ 
+	$trunc_num = 0;
+	$degrees_k = 0;
+	$volume = 0;
+	$velocity = 0;
+	
+}
+$form_layout = <<< EOD
+<p>
+<form method="post" action="">
+Floating point number to be truncated: <input type="text" name="float_value">
+<br>Temperature in degrees Fahrenheit: <input type="text" name="degrees_f">
+<br>Legth of one side of you die: <input type="text" name="area">
+<br>Height you fell from: <input type="text" name="height">
+<br><input type="submit" value="Calculate" name="submit">
+</form>
+</p>
+EOD;
+
+$form_results = "<p> \nTruncated Number: " . $trunc_num . "<br> \nTemperature in Degrees Kelvin: " . 
+$degrees_k . "<br> \nThe Volume of Your Dodecahedron Die: " . $volume . 
+"<br> \nHow Fast You Will Hit the Ground: " . $velocity . "<br>\n</p>\n";
 ?>
 
 <!DOCTYPE html>
@@ -58,6 +96,15 @@ function impactVelocity($height)
     <title>Class 4 Lab</title>
 </head>
 <body>
-
+<?php
+	if (!isset($_POST['submit'])) 
+	{ 
+		echo $form_layout;
+	} 
+	else 
+	{
+		echo $form_results;
+	}
+?>
 </body>
 </html>
